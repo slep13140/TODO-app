0365483dd724d6fad68cb80ef4c14c3d812fb377
Sergey Slepukhin <s@rdr13.ru>
2022-10-28 19:28:10 +0300

    Correct components Task and Task-list

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Task from "../task";
 
@@ -8,17 +8,14 @@ import { formatDistanceToNow } from "date-fns";
 
 const createDate = formatDistanceToNow(new Date());
 
-const TaskList = ({ todos }) => {
-  const elements = todos.map((item) => {
-    const { classList, id, editing, ...itemProps } = item;
-    return (
-      <li key={id} className={classList}>
-        <Task {...itemProps} />
-        <input type="text" className="edit" defaultValue="Editing task" />
-      </li>
-    );
-  });
-  return <ul className="todo-list">{elements}</ul>;
-};
+export default class TaskList extends Component {
+  render() {
+    const { todos } = this.props;
 
-export default TaskList;
+    const elements = todos.map((item) => {
+      const { id, ...itemProps } = item;
+      return <Task {...itemProps} key={id} />;
+    });
+    return <ul className="todo-list">{elements}</ul>;
+  }
+}
