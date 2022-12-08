c3e45bb423e05a30cc4f35d4c3efe3fd96e21cab
Sergey Slepukhin <s@rdr13.ru>
2022-12-08 22:26:12 +0300

    Converted Task into hooks

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,118 +1,95 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 
 import './Task.css'
 
-export default class Task extends Component {
-  constructor() {
-    super()
-    this.state = { editingTask: '' }
-    this.inputRef = React.createRef()
-    this.onTaskChange = (e) => {
-      this.setState({
-        editingTask: e.target.value,
-      })
-    }
-    this.onSubmit = (e) => {
-      const { keyId, onNewTask } = this.props
-      const { editingTask } = this.state
-      if (e.code === 'Enter') {
-        if (editingTask.length === 0 || editingTask.trim().length === 0) {
-          return
-        }
+function Task(props) {
+  const [editingTask, setEditingTask] = useState('')
+  const inputRef = useRef()
 
-        onNewTask(keyId, editingTask)
+  const { completed, filterCompleted, checked } = props
+  const { onToggleCompleted, keyId, description } = props
+  const { created, onDeleted, onEditing } = props
+  const { doTimer, doPause, editing } = props
+  const { timerMin, timerSec, onNewTask } = props
+  const keyLabel = `${keyId} + 'Id'`
+  const createTime = `created ${created} ago`
+  const timer = `${timerMin}:${timerSec}`
+
+  useEffect(() => setEditingTask(description), [])
+
+  const onSubmit = (e) => {
+    if (e.code === 'Enter') {
+      if (editingTask.length === 0 || editingTask.trim().length === 0) {
+        return
       }
+      onNewTask(keyId, editingTask)
     }
   }
 
-  componentDidMount() {
-    const { description } = this.props
-    this.setState({
-      editingTask: description,
-    })
+  let classNames = ''
+  if (completed) {
+    classNames += 'completed'
+  }
+  if (filterCompleted && completed) {
+    classNames += ' filterCompleted'
+  } else if (filterCompleted && !completed) {
+    classNames += 'filterCompleted'
+  }
+  if (editing) {
+    classNames += ' editing'
+    inputRef.current.focus()
   }
 
-  render() {
-    const { editingTask } = this.state
-    const { completed, filterCompleted, checked } = this.props
-    const { onToggleCompleted, keyId, description } = this.props
-    const { created, onDeleted, onEditing } = this.props
-    const { doTimer, doPause, editing } = this.props
-    const { timerMin, timerSec } = this.props
-    const keyLabel = `${keyId} + 'Id'`
-    const createTime = `created ${created} ago`
-    const timer = `${timerMin}:${timerSec}`
-
-    let classNames = ''
-    if (completed) {
-      classNames += 'completed'
-    }
-    if (filterCompleted && completed) {
-      classNames += ' filterCompleted'
-    } else if (filterCompleted && !completed) {
-      classNames += 'filterCompleted'
-    }
-    if (editing) {
-      classNames += ' editing'
-      this.inputRef.current.focus()
-    }
-    return (
-      <li className={classNames}>
-        <div className="view">
-          <input className="toggle" id={keyLabel} type="checkbox" checked={checked} onChange={onToggleCompleted} />
-          <label htmlFor={keyLabel}>
-            <span className="description" role="presentation" onClick={onToggleCompleted}>
-              {description}
-            </span>
-            <span className="description description-timer">
-              <button
-                type="button"
-                className="icon-play"
-                aria-label="play-timer"
-                title="play timer"
-                onClick={doTimer}
-              />
-              <button
-                type="button"
-                className="icon-pause"
-                aria-label="pause-timer"
-                title="pause timer"
-                onClick={doPause}
-              />
-              <span className="timer">{timer}</span>
-            </span>
-            <span className="created">{createTime}</span>
-          </label>
-          <button
-            type="button"
-            className="icon icon-edit"
-            aria-label="icon-edit"
-            title="editing task"
-            onClick={onEditing}
-          />
-          <button
-            type="button"
-            className="icon icon-destroy"
-            aria-label="icon-destroy"
-            title="destroy task"
-            onClick={onDeleted}
-          />
-        </div>
-        <label htmlFor="edit" className="editLabel">
-          <input
-            type="text"
-            id="edit"
-            className="edit"
-            onChange={this.onTaskChange}
-            onKeyPress={this.onSubmit}
-            value={editingTask}
-            ref={this.inputRef}
-          />
+  return (
+    <li className={classNames}>
+      <div className="view">
+        <input className="toggle" id={keyLabel} type="checkbox" checked={checked} onChange={onToggleCompleted} />
+        <label htmlFor={keyLabel}>
+          <span className="description" role="presentation" onClick={onToggleCompleted}>
+            {description}
+          </span>
+          <span className="description description-timer">
+            <button type="button" className="icon-play" aria-label="play-timer" title="play timer" onClick={doTimer} />
+            <button
+              type="button"
+              className="icon-pause"
+              aria-label="pause-timer"
+              title="pause timer"
+              onClick={doPause}
+            />
+            <span className="timer">{timer}</span>
+          </span>
+          <span className="created">{createTime}</span>
         </label>
-      </li>
-    )
-  }
+        <button
+          type="button"
+          className="icon icon-edit"
+          aria-label="icon-edit"
+          title="editing task"
+          onClick={onEditing}
+        />
+        <button
+          type="button"
+          className="icon icon-destroy"
+          aria-label="icon-destroy"
+          title="destroy task"
+          onClick={onDeleted}
+        />
+      </div>
+      <label htmlFor="edit" className="editLabel">
+        <input
+          type="text"
+          id="edit"
+          className="edit"
+          onChange={(e) => setEditingTask(e.target.value)}
+          onKeyDown={onSubmit}
+          value={editingTask}
+          ref={inputRef}
+        />
+      </label>
+    </li>
+  )
 }
 
 Task.defaultProps = {
@@ -134,3 +111,4 @@ Task.propTypes = {
   onEditing: PropTypes.func.isRequired,
   editing: PropTypes.bool.isRequired,
 }
+export default Task
